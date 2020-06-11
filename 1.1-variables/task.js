0fd0a2a558748c14326cceda8865b8eecd1f5990
Emma Danielyan <emma.danielyan90@gmail.com>
2020-06-11 14:42:41 +0400

    Corrected teacher comments

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -4,13 +4,14 @@
    2) После ключевого слова return вместо комментария необходимо написать переменную либо выражение с ответом.
    3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
 */
-let algebra = a;
-let geography = g;
-let physics = p;
+
 
 function averageMark(a, g, p) {
 
 	// реализуйте здесь задание №1
+	let algebra = a;
+	let geography = g;
+	let physics = p;
 	let sum = a + g + p;
 	let result = sum / 3;
 
