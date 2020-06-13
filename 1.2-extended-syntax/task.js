ab5031bf7abcc848550bb76ed410e28a58edb9f8
Emma Danielyan <emma.danielyan90@gmail.com>
2020-06-14 00:33:46 +0400

    Initial commit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,57 @@
+'use strict';
+// 2x^2+4x-3=0
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
-    // return x;
-}
+    a = 2;
+    b = 4;
+    c = -3;
+    let x = [];
+    let descr = b ** 2 - 4 * a * c;
+    if (descr > 0) {
+        let x1 = (-b + Math.sqrt(descr))/(2 * a);
+        let x2 = (-b - Math.sqrt(descr))/(2 * a);
+        return ('x1 = '+x1+'   x2 = '+x2);
+    } if (descr < 0) {
+        alert('Решений нет, так как дискриминант меньше нуля.');
+    } if (descr == 0) {
+        x1 = (-b)/(2 * a);
+        return ('x = '+x1);
+    };
+};
+    // let result = [a, b, c];
 
-function getAverageMark(marks){
+    // return x;
+
+function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark;
+    if (marks.length === 0) {
+        return 0;
+    } else if (marks.length > 5) {
+        marks.splice(5);
+        return 'Вы ввели более пяти оценок! Расчет будет только на первые пять оценок';
+    }
+    let sum = 0;
+    for (let i = 0; i < 5; i++) {
+        sum += marks[i];
+    }
+    let averageMark = sum / marks.length;
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-    // return result;
+    let year = new Date().getFullYear()-18;
+    if (dateOfBirthday.getFullYear() > year) {
+        return ('Сожалею, ' + name + ',' + ' но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!');
+    }else{
+        return ('Не желаете ли олд-фэшн ' + name);
+    }
 }
+    // Чтобы получить количество лет на основе только года:
+    // let get_current_age = function(date) {
+    //     return new Date().getFullYear() - date;
+    // };
+    // alert(get_current_age(1986));
+
+
+
