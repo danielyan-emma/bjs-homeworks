f086fb05abcc1d0e1e07e6b6b70cdf35e41e0359
Emma Danielyan <emma.danielyan90@gmail.com>
2020-06-15 02:31:22 +0400

    Work in comment teacher

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,19 +2,20 @@
 // 2x^2+4x-3=0
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
-    // a = 2;
-    // b = 4;
-    // c = -3;
     let descr = b ** 2 - 4 * a * c;
+    if (descr < 0) {
+        return('Решений нет, так как дискриминант меньше нуля.' + []);
+    }
+
     let x1 = (-b + Math.sqrt(descr)) / (2 * a);
     let x2 = (-b - Math.sqrt(descr)) / (2 * a);
-    if (descr > 0) {
-        return [x1, x2];
-    } if (descr < 0) {
-        return ('Решений нет, так как дискриминант меньше нуля.');
-    } if (descr == 0) {
+    if (descr === 0) {
         return [x1];
     };
+
+    if (descr > 0) {
+        return [x1, x2];
+    }
 };
 
 
@@ -24,30 +25,30 @@ function getAverageMark(marks) {
         return 0;
     } else if (marks.length > 5) {
         marks.splice(5);
-        return 'Вы ввели более пяти оценок! Расчет будет только на первые пять оценок';
+        console.log('Вы ввели более пяти оценок! Расчет будет только на первые пять оценок');
     }
     let sum = 0;
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < marks.length; i++) {
         sum += marks[i];
     }
-    let averageMark = sum / marks.length;
-    return averageMark;
+    return  sum / marks.length;
 }
+
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
-    let year = new Date().getFullYear()-18;
-    if (dateOfBirthday.getFullYear() > year) {
-        return ('Сожалею, ' + name + ',' + ' но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!');
+    let nowYear = new Date().getFullYear();
+    let userYear = dateOfBirthday.getFullYear();
+    if ((nowYear - userYear) > 18) {
+        return (`Не желаете ли олд-фэшн ${name}`);
     }else{
-        return ('Не желаете ли олд-фэшн ' + name);
+        return (`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
     }
 }
-    // Чтобы получить количество лет на основе только года:
-    // let get_current_age = function(date) {
-    //     return new Date().getFullYear() - date;
-    // };
-    // alert(get_current_age(1986));
 
-
+// Чтобы получить количество лет на основе только года:
+// let get_current_age = function(date) {
+//     return new Date().getFullYear() - date;
+// };
+// alert(get_current_age(1986));
 
