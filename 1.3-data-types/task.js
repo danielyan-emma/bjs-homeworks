2cb807a70396960ba45ef1e6a016828d38222c1d
Emma Danielyan <emma.danielyan90@gmail.com>
2020-06-17 16:31:12 +0400

    Task 1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,7 +1,27 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (isNaN(percent)) {
+        console.log(`Параметр "процентная ставка" содержит неправильное значение ${percent}`);
+    } else if (isNaN(contribution)) {
+        console.log(`Параметр "начальный взнос" содержит неправильное значение ${contribution}`);
+    } else if (isNaN(amount)) {
+        console.log(`Параметр "общая стоимость" содержит неправильное значение ${amount}`);
+    } else if (isNaN(date)) {
+        console.log(`Параметр "срок ипотеки" содержит неправильное значение ${date}`);
+    } else {
+        let refundBank = amount - contribution;
+        let monthPaid = ((date.getFullYear() - new Date().getFullYear()) * 12) + (date.getMonth() - new Date().getMonth());
+        let p = (percent / 100) / 12;
+        let monthlyFee = refundBank * (p + p / (((1 + p) ** monthPaid) - 1));
+        let totalAmount = parseFloat((monthPaid * monthlyFee).toFixed(2));
+        console.log(totalAmount);
+        return totalAmount;
+    }
 }
+
+
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
