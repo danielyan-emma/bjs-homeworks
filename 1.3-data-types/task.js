36c54e0b98c9d58d3235fddb088e09ee6082faeb
Emma Danielyan <emma.danielyan90@gmail.com>
2020-06-19 16:39:54 +0400

    Work on Edgar's comments

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -3,19 +3,22 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
     if (isNaN(percent)) {
-        console.log(`Параметр "процентная ставка" содержит неправильное значение ${percent}`);
-    } else if (isNaN(contribution)) {
-        console.log(`Параметр "начальный взнос" содержит неправильное значение ${contribution}`);
-    } else if (isNaN(amount)) {
-        console.log(`Параметр "общая стоимость" содержит неправильное значение ${amount}`);
-    } else if (isNaN(date)) {
-        console.log(`Параметр "срок ипотеки" содержит неправильное значение ${date}`);
+        return `Параметр "процентная ставка" содержит неправильное значение ${percent}`;
+    }
+    if (isNaN(contribution)) {
+        return `Параметр "начальный взнос" содержит неправильное значение ${contribution}`;
+    }
+    if (isNaN(amount)) {
+        return `Параметр "общая стоимость" содержит неправильное значение ${amount}`;
+    }
+    if (isNaN(date)) {
+        return `Параметр "срок ипотеки" содержит неправильное значение ${date}`;
     } else {
-        let refundBank = amount - contribution;
-        let monthPaid = ((date.getFullYear() - new Date().getFullYear()) * 12) + (date.getMonth() - new Date().getMonth());
-        let p = (percent / 100) / 12;
-        let monthlyFee = refundBank * (p + p / (((1 + p) ** monthPaid) - 1));
-        let totalAmount = parseFloat((monthPaid * monthlyFee).toFixed(2));
+        const refundBank = amount - contribution;
+        const monthPaid = ((date.getFullYear() - new Date().getFullYear()) * 12) + (date.getMonth() - new Date().getMonth());
+        const p = (percent / 100) / 12;
+        const monthlyFee = refundBank * (p + p / (((1 + p) ** monthPaid) - 1));
+        const totalAmount = parseFloat((monthPaid * monthlyFee).toFixed(2));
         console.log(totalAmount);
         return totalAmount;
     }
@@ -25,10 +28,9 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    if (name === '' || name === null || typeof name === 'undefined') {
-        name = 'Аноним';
+    if (name === '' || name === null || typeof name === undefined) {
     }
-    return (`Привет, мир! Меня зовут ${name}`);
+    return (`Привет, мир! Меня зовут ${name || "Аноним"}`);
 }
 
 // if (name === '' || name === 'null' || name === 'undefined') {
@@ -37,4 +39,7 @@ function getGreeting(name) {
 // return (`Привет, мир! Меня зовут ${name}`);
 // }
 // }
+
+
+
 
